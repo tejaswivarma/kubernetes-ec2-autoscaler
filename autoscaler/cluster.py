5d7a2fc3b2502ed6818691d6cc915680618629c2
Vicki Cheung <vicki@openai.com>
2017-01-03 17:52:27 -0800

    less grace period for instances

--- autoscaler/cluster.py
+++ autoscaler/cluster.py
@@ -59,7 +59,7 @@ class Cluster(object):
     # since we pay for the full hour, don't prematurely kill instances
     # the number of minutes into the launch hour at which an instance
     # is fine to kill
-    LAUNCH_HOUR_THRESHOLD = 60 * 50  # 50 minutes
+    LAUNCH_HOUR_THRESHOLD = 60 * 40  # 40 minutes
 
     # HACK: before we're ready to favor bigger instances in all cases
     # just prioritize the ones that we're confident about
